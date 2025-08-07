93ed1e649e3fc82ac4e05b9ec1b038b57fc1c5ce
Alexandre Strube <a.strube@fz-juelich.de>
2025-08-07 16:52:51 +0200

    Run in both api servers for now

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source venv/bin/activate
-python main.py
+OPENAI_BASE_URL=https://api.helmholtz-blablador.fz-juelich.de/v1 python main.py
 
+OPENAI_BASE_URL=http://134.94.199.27:8080/v1 python main.py
